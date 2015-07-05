8bd9eba96072e25d0fc319a8e509e3dfb15035bd
sadaya-nishio <cy.nishio0820@gmail.com>
2015-07-05 13:01:59 +0900

    ver.1.0.0 commit.

--- kintone_sdk4python/sample/sample.py
+++ kintone_sdk4python/sample/sample.py
@@ -1,77 +1,79 @@
 #!/usr/bin/env python
 # -*- coding: utf_8 -*-
-'''
+
+import sys
 from kintone_sdk4python import Kintone
 
 kintone = Kintone()
 
-kintone.set_domain('hw1xj.cybozu.com')
-#kintone.set_basic_auth(id, password)
-kintone.set_user_auth('sadaya-nishio', 'sadaya-nishio')
-#kintone.set_token_auth(token)
+kintone.set_domain('example.cybozu.com')
+kintone.set_basic_auth('id', 'password')
+kintone.set_user_auth('login_name', 'password')
+kintone.set_token_auth('token')
 
 #GET
-get_record_resp = kintone.get_record(328, 3493)
-get_records_resp = kintone.get_records(328, '', ['$id'])
+get_record_resp = kintone.get_record(10, 1)
+get_records_resp = kintone.get_records(10, 'レコード番号 > 10', ['$id', '作成日時'])
+get_all_records_resp = kintone.get_records(10, '', ['$id', '作成日時'], True)
 
 #POST
 post_record = {
-    'タイトル': {
-            'value': 'hoge'
-        }
+    'title': {
+        'value': 'title1'
+    }
 }
 post_records = [
     {
-        'タイトル': {
-            'value': 'hoge'
+        'title': {
+            'value': 'title1'
         }
     },
     {
-        'タイトル': {
-            'value': 'bar'
+        'title': {
+            'value': 'title2'
         }
     }
 ]
-post_record_resp = kintone.post_record(328, post_record)
-post_records_resp = kintone.post_records(328, post_records)
+post_record_resp = kintone.post_record(10, post_record)
+post_records_resp = kintone.post_records(10, post_records)
 
 #PUT
 put_record = {
-    'タイトル': {
-        'value': 'hoge'
+    'title': {
+        'value': 'change_title'
     }
 }
 put_records = [
     {
-        'id': 3493,
+        'id': 1,
         'record': {
-            'タイトル': {
-                'value': 'title1'
+            'title': {
+                'value': 'change_title1'
             }
         }
     },
     {
-        'id': 3492,
+        'id': 2,
         'record': {
-            'タイトル': {
-                'value': 'title2'
+            'title': {
+                'value': 'change_title1'
             }
         }
     }
 ]
-put_record_resp = kintone.put_record(328, 3493, put_record)
-put_records_resp = kintone.put_records(328, put_records)
+put_record_resp = kintone.put_record(10, 1, put_record)
+put_records_resp = kintone.put_records(10, put_records)
 
 #DELETE
-del_records_resp = kintone.del_records(328, [3490])
+del_records_resp = kintone.del_records(10, [1, 2, 3])
 
-
-print(record_resp)
+print(get_record_resp)
 print(get_records_resp)
 print(len(get_records_resp['records']))
-print(del_records_resp)
+print(get_all_records_resp)
+print(len(get_all_records_resp['records']))
 print(post_record_resp)
 print(post_records_resp)
 print(put_record_resp)
 print(put_records_resp)
-'''
+print(del_records_resp)
