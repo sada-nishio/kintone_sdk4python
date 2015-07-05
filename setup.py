802b463c52a3a4d038451d0694c5f02a4e774379
sadaya-nishio <cy.nishio0820@gmail.com>
2015-07-05 09:39:05 +0900

    5th commit.

--- setup.py
+++ setup.py
@@ -3,18 +3,15 @@
 from setuptools import setup, find_packages
 from kintone_sdk4python import __author__, __version__, __license__
 
-
-"""
 setup(
-    name = 'kintone-sdk for python',
+    name = 'kintone SDK for Python',
     version = __version__,
-    description = 'Sample for installing python library from github using pip',
+    description = 'kintone SDK for Python.',
     license = __license__,
     author = __author__,
-    author_email = 'junion@junion.org',
-    url = 'https://github.com/junion-org/pip_github_test.git',
-    keywords = 'sample pip github python',
+    author_email = 'cy.nishio0820@gmail.com',
+    url = 'https://github.com/sada-nishio/kintone_sdk4python.git',
+    keywords = 'kintone SDK for Python.',
     packages = find_packages(),
-    install_requires = [],
+    install_requires = ['httplib2']
 )
-"""
